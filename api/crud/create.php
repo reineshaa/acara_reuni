72c90b11cd254855d70fbd553508e9786e95ae80
reineshaa <ratudasha.7@gmail.com>
2025-08-21 19:23:50 +0700

    updated code

--- api/crud/create.php
+++ api/crud/create.php
@@ -1,8 +1,17 @@
 <?php
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+
 
 include("helper.php");
+
+if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+    http_response_code(200);
+    exit();
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     include("../../connect.php");
--- api/crud/index.php
+++ api/crud/index.php
@@ -1,6 +1,8 @@
 <?php
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
 include("helper.php");
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
